3e159c7be7829173740baaf8d5206b0852cb2249
Sami Reimavuo <sami@reimavuo.net>
2022-11-13 19:16:40 +0200

    Lint ei pitäisi mennä läpi nyt

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,11 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        self.tilavuus = max(0.0, tilavuus)
+        #self.tilavuus = max(0.0, tilavuus)
+
+        if tilavuus < 0.0:
+            self.tilavuus = 0.0
+        else:
+            self.tilavuus = tilavuus
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
